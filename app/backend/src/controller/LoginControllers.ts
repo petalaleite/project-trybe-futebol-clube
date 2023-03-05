14c9d63d8c96ce758162d259f46018c3e4f392d2
Pétala Leite <petalaleite@gmail.com>
2023-03-05 17:31:49 -0300

    teste

--- app/backend/src/controller/LoginControllers.ts
+++ app/backend/src/controller/LoginControllers.ts
@@ -1,21 +1,21 @@
-// import { Request, Response } from 'express';
-// import jwt, { SignOptions } from 'jsonwebtoken';
-// import UserService from '../service/userServices';
+import { Request, Response } from 'express';
+import * as jwt from 'jsonwebtoken';
+import UserService from '../service/userServices';
 
-// const jwtConfig: SignOptions = {
-//   expiresIn: '2d',
-//   algorithm: 'HS256',
-// };
+const jwtConfig: jwt.SignOptions = {
+  expiresIn: '2d',
+  algorithm: 'HS256',
+};
 
-// const secret = process.env.JWT_SECRET;
+const secret = process.env.JWT_SECRET || 'tolkien';
 
-// export default class LoginController {
-//   constructor(private userService = new UserService()) {}
+export default class LoginController {
+  constructor(private userService = new UserService()) {}
 
-//   login = async (req: Request, res: Response) => {
-//     const { email, password } = req.body;
-//     const userLogin = await this.userService.getEmailAndPassword(email, password);
-//     const token = jwt.sign({ data: userLogin }, secret, jwtConfig);
-//     res.status(200).json({ token });
-//   };
-// }
+  login = async (req: Request, res: Response) => {
+    const { email, password } = req.body;
+    const userLogin = await this.userService.getEmailAndPassword(email, password);
+    const token = jwt.sign({ data: userLogin }, secret, jwtConfig);
+    res.status(200).json({ token });
+  };
+}
