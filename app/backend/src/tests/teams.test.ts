7f6340d7593dabd9b6f1bd39fd80e1661b839421
Pétala Leite <petalaleite@gmail.com>
2023-03-10 16:37:12 -0300

    testando o teste

---
+++ app/backend/src/tests/teams.test.ts
@@ -0,0 +1,20 @@
+import * as sinon from 'sinon';
+import * as chai from 'chai';
+// @ts-ignore
+import chaiHttp = require('chai-http');
+import { app } from '../app'
+import { Model } from 'sequelize';
+chai.use(chaiHttp);
+const { expect } = chai;
+
+describe('Teste de integração a rota teams', function () {
+  it('Testa a rota /teams com sucesso', async function () {
+    const result = await chai.request(app).get('/teams')
+    expect(result.status).to.be.equal(200)
+  })
+
+  afterEach(function () {
+    sinon.restore()
+
+  })
+})
